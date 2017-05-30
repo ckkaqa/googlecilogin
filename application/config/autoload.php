41e24818a7e2f55debd4baddcd0688292e592af0
Jhunnie <jhunnie.ramos20@gmail.com>
2017-05-30 09:53:51 +0800

    get total work days function

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'hash', 'form');
+$autoload['helper'] = array('url', 'hash', 'form', 'payroll');
 
 
 /*
